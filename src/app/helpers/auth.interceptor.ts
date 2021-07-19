8bf9af3974dfffb60d6b4e5aea810cba37d9cda2
Edison MASSENA <massenaedison0@outlook.fr>
2021-07-19 14:53:06 +0200

    'cors-header'

--- src/app/helpers/auth.interceptor.ts
+++ src/app/helpers/auth.interceptor.ts
@@ -15,6 +15,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
     const token = this.token.getToken();
+    authReq = req.clone({ headers: req.headers.set('Access-Control-Allow-Origin', 'https://tabprojet.herokuapp.com/assets/')});
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
     }
