b55f1cac8109de6b3b16d8c9e0deb126c650c12d
Edison MASSENA <massenaedison0@outlook.fr>
2021-11-10 09:50:32 +0100

    ngupdate

--- src/app/components/tableau/tableau.component.ts
+++ src/app/components/tableau/tableau.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, HostListener, NgZone } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, NgZone, Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
