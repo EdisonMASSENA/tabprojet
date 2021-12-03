c051f1257e59024db42496e97b05e9d4375a64cb
Edison MASSENA <massenaedison0@outlook.fr>
2021-12-03 10:08:15 +0100

    envurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  Url: 'https://api-tab-dev.herokuapp.com/'
-  // Url: 'https://api-tab.herokuapp.com/'
+  // Url: 'https://api-tab-dev.herokuapp.com/'
+  Url: 'https://api-tab.herokuapp.com/'
 };
